76cb4d63e2c2c4ab5d1a5fa8abf1260974b6ffca
unknown <monmaramba@yahoo.com>
2018-09-11 17:52:27 -0700

    apiroutes.js updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,43 @@
 var db = require("../models");
-var express = require('express');
-var path = require('path');
-
-//var db = require("../../models");
-
-//create Router instance by invoking method
-var router = express.Router();
-
-var bodyParser = require("body-parser");
-var parseUrlEncoded = bodyParser.urlencoded({
-    extended: false
-});
-
 
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+//to get latest entry in table 2
+var latestUser;
+var latestUserScore;
+db.response.findAll({
+  limit: 1,
+  order: [["createdAt", "DESC"]]
+}).then(function(user){
+  latestUser = user.id;
+  latestUserScore = (user.score1 + user.score2 + user.score3 + user.score4 + user.score5);
+  console.log(latestUser);
+  console.log(latestUserScore);
+}).then(function(latestUserScore){
+  return latestUserScore;
+  
+});
+
+function bestMatch() {
+  //get all heros
+  db.hero.findAll().then(function(heroArr){
+  //setting default values     
+    var heroUserDifference = Infinity;
+    var heroIndex = 0; //default hero match is array position 0
+    var heroMatch = heroArr[heroIndex];
+    
+    for (var i = 0; i < heroArr.length; i++) {
+      let heroScoreVariance = Math.abs(heroArr[i].score - latestUserScore);
+      if (heroScoreVariance < heroUserDifference) {
+        heroUserDifference = heroScoreVariance;
+        heroIndex = i;
+        };
+    }
+    return heroMatch;
+    
+  })
+}
+bestMatch();
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
@@ -62,7 +79,7 @@ function bestMatch(callback) {
       
     }
     //compare heroScoreVariance with heroUserDifference and assign //lower to heroUserDifference; Set hero id to [i].
-    //if (heroScoreVariance < heroUserDifference) {
+    if (heroScoreVariance < heroUserDifference) {
       heroUserDifference = heroScoreVariance;
       heroIndex = i;
       
