baf2d19e0b0796afa2440988c99e602a5f8d7521
chriskelsey <chriskelsey.net@gmail.com>
2018-09-15 00:36:17 -0700

    Fixing syntax error

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ db.sequelize.sync(syncOptions).then(function () {
   app.listen(PORT, function () {
     console.log("Listening on port "+ PORT);
   });
-});.then(function() {
+}).then(function() {
   db.hero.bulkCreate(heroes);
   db.response.bulkCreate(responses);
 });
